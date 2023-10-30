16d339dcc8a473c15a5942b816d950d229ebe43c
takkar94 <ghimireaditya@gmail.com>
2023-10-30 20:25:17 +0545

    creating a basic api

--- app.js
+++ app.js
@@ -1,14 +1,63 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res)=> {
-    res.status(200).json({ message: 'Hello from the server side' , app: 'Natours'});
+app.use(express.json());
+
+// app.get('/', (req, res)=> {
+//     res.status(200).json({ message: 'Hello from the server side' , app: 'Natours'});
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-//app
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
 
-const port = 3000
-app.listen(port, ()=> {
-    console.log(`running on ${port} ......`);
+
+    res.status(200).json({
+      status: 'success',
+    //   results: tours.length,
+    //   data: {
+    //     tours,
+    //   },
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = { id: newID, ...req.body };
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`running on ${port} ......`);
 });
