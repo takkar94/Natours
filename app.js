c7e520137ff9069f1dd5743e85f6467efe83ce95
takkar94 <ghimireaditya@gmail.com>
2023-10-31 10:27:55 +0545

    usage of basic middleware

--- app.js
+++ app.js
@@ -4,14 +4,28 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
+
+app.use((req, res, next)=> {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
+
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
 
 const getAllTours =  (req, res) => {
+  console.log(req.requestTime);
     res.status(200).json({
       status: 'success',
+      requestedAt: req.requestTime,
       results: tours.length,
       data: {
         tours,
@@ -43,8 +57,7 @@ const getTour = (req, res) => {
 
 
 const createTour = (req, res) => {
-    //console.log(req.body);
-  
+ 
     const newID = tours[tours.length - 1].id + 1;
     const newTour = { id: newID, ...req.body };
   
@@ -95,16 +108,28 @@ const deleteTour = (req,res) => {
     })
 };
 
+/////////////////////////////////////////////////////////////////////////////////////////
+//routes
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
 
 
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTour);
-app.post('/api/v1/tours', createTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
+app
+    .route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTour);
+
+app
+    .route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour);
 
 
-
+//server
 const port = 3000;
 app.listen(port, () => {
   console.log(`running on ${port} ......`);
