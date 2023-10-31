148d4e302b47f28cd3e9db6861e479665eb29095
takkar94 <ghimireaditya@gmail.com>
2023-10-31 12:26:08 +0545

    simple body check

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -18,6 +18,17 @@ exports.checkID = (req, res, next, val) => {
 }
 
 
+exports.checkBody = (req, res, next) => {
+  if(!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'Fail',
+      message: 'missing name or price'
+    });
+  }
+  next();
+}
+
+
 exports.getAllTours =  (req, res) => {
     console.log(req.requestTime);
       res.status(200).json({
