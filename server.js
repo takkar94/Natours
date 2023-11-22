388465eb662dd9c4f6be6c8e8fcc48849e31b6e1
takkar94 <ghimireaditya@gmail.com>
2023-11-22 13:19:38 +0545

    using mongoose to ocnnect to the database

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    //console.log(con.connection);
+    console.log('DB Connection successful');
+  });
 
 const app = require('./app');
 
