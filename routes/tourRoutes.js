7456b0c8cb9242356a5e5fc26638921c17a81629
takkar94 <ghimireaditya@gmail.com>
2023-12-08 12:57:21 +0545

    agg pipeline uniwinding projection

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,7 @@ tourRouter
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
 tourRouter.route('/tour-stats').get(tourController.getTourStats);
+tourRouter.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 tourRouter
   .route('/')
