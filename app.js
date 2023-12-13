9eb7a59112753e34e73bc7fa4a4747acae1f7dcb
takkar94 <ghimireaditya@gmail.com>
2023-12-13 12:22:47 +0545

    adding a global error handler

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+/* eslint-disable prettier/prettier */
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -14,8 +17,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -25,11 +26,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', (req, res, next)=> {
-  res.status(404).json({
-    status:'fail',
-    message: `can't find ${req.originalUrl} on this server`
-  })
-})
+app.all('*', (req, res, next) => {
+
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
