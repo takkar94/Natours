ce30519787c2c4b2adbb74f15107060faac8bb9b
takkar94 <ghimireaditya@gmail.com>
2023-12-26 10:47:12 +0545

    auth

--- models/userModel.js
+++ models/userModel.js
@@ -10,20 +10,21 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A user must have a name'],
-    trim: true,
+    trim: true
   },
   email: {
     type: String,
     required: [true, 'A user must have an email'],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email'],
+    validate: [validator.isEmail, 'Please provide a valid email']
   },
   photo: String,
   password: {
     type: String,
     required: [true, 'A user must have a password'],
     minlength: [8, 'Password must be more than or equal to 8 characters'],
+    select: false
   },
   passwordConfirm: {
     type: String,
@@ -32,19 +33,22 @@ const userSchema = new mongoose.Schema({
       validator: function(el) {
         return el === this.password;
       },
-      message: 'Passwords are not the same',
+      message: 'Passwords are not the same'
     }
   }
 });
 
-userSchema.pre('save', async function(next){
-  if(!this.isModified('password')) return next();
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
 
   this.password = await bcrypt.hash(this.password, 12);
 
   this.passwordConfirm = undefined;
+});
 
-})
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword){
+  return bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model('User', userSchema);
 
