7e98ecfa3694f9e34fb3a2548e3e0488011d4881
takkar94 <ghimireaditya@gmail.com>
2024-01-06 12:20:07 +0545

    sneding a JWT via a Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,17 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true
+  }
+
+  if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined; //in order to remove password during signup output 
 
   res.status(statusCode).json({
     status: 'Success',
@@ -170,7 +181,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   user.passwordResetToken = undefined;
   user.passwordResetExpires = undefined;
   await user.save();
-  
+
   createSendToken(user, 200, res);
 });
 
