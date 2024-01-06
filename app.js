c5f2ee4f9baae4254909de8f6c9f5525a1fa8261
takkar94 <ghimireaditya@gmail.com>
2024-01-06 12:28:15 +0545

    added rate limiter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,10 +10,17 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1)Global  MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowM: 60 * 60 * 1000, //will need to modify the time later 
+  message: 'Too many requests from this IP, please try again in an hour' 
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
