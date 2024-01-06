26e6a30ac6af6c1c518d0d77e40afb6aee8e55d9
takkar94 <ghimireaditya@gmail.com>
2024-01-06 12:32:54 +0545

    security http headers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,6 +12,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1)Global  MIDDLEWARES
+
+app.user(helmet());//securing http headers
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -22,7 +26,9 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+
+
+app.use(express.json({limit: '10kb'}));
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
