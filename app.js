fe86cb67c890daa8f8db337bb358d890b0c0097d
takkar94 <ghimireaditya@gmail.com>
2024-01-06 12:52:58 +0545

    data sanitization

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -13,7 +16,7 @@ const app = express();
 
 // 1)Global  MIDDLEWARES
 
-app.user(helmet());//securing http headers
+app.use(helmet());//securing http headers
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -29,6 +32,13 @@ app.use('/api', limiter);
 
 
 app.use(express.json({limit: '10kb'}));
+
+// Data Sanitization for nosql query injection attacks
+app.use(mongoSanitize());
+
+//data sanitization against XSS
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
