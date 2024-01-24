4838804e3899725209419d3f6d3a195ec5438410
takkar94 <ghimireaditya@gmail.com>
2024-01-24 16:19:37 +0545

    modeling reviews

---
+++ models/reviewModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+//const slugify = require('slugify');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      trim: true,
+      required: [true, 'Review Cannot be empty']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'review must belong to a user']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
