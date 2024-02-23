5954cf87521a7afaa7d29bd8f9f0d6661c8a4df2
takkar94 <ghimireaditya@gmail.com>
2024-02-23 20:29:05 +0545

    created the average rating calculater within the model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-//const slugify = require('slugify');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -50,6 +50,30 @@ reviewSchema.pre(/^find/, function(next) {
     select: ' name photo'
   });
   next();
+});
+
+reviewSchema.statics.calcAverageRating = async function(tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  console.log(stats);
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating
+  });
+};
+
+reviewSchema.post('save', function(next) {
+  this.constructor.calcAverageRating(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
