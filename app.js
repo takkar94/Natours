86bb453e94eb4577ef82abd9a16fe1c1cbecde87
takkar94 <ghimireaditya@gmail.com>
2024-03-18 11:11:17 +0545

    pug usage

--- app.js
+++ app.js
@@ -12,14 +12,13 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const reviewRouter= require('./routes/reviewRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 // 1)Global  MIDDLEWARES
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet()); //securing http headers
@@ -56,8 +55,6 @@ app.use(
   })
 );
 
-
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   //console.log(req.headers);
@@ -66,9 +63,12 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 
-app.get('/', (req,res)=> {
-  res.status(200).render('base')
-})
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Aaditya'
+  });
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
