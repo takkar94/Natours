5ff78d3ac40a2754d2ae9e35f2f32f72058847a6
takkar94 <ghimireaditya@gmail.com>
2024-03-19 16:25:19 +0545

    refactoring for views

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoute');
 
 const app = express();
 
@@ -62,29 +63,11 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Aaditya'
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  });
-})  
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'The Forest Hiker'
-  });
-})
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+
 
 app.all('*', (req, res, next) => {
   next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
