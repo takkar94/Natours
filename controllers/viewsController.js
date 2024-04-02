53f50766c597b9bbf2ecccb32dc1b43a30d78098
takkar94 <ghimireaditya@gmail.com>
2024-04-02 11:50:24 +0545

    card holder template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  //getting tour data first then render the template
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('overview', {
